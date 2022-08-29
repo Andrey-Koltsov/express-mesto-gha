106c097142824e4ac618a3f99132998ad7ba1ad2
Andrey Koltsov <a.koltcov@itr24.com>
2022-08-29 16:47:50 +0300

    feat: fix autotest

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,6 +26,22 @@ const createCard = async (req, res) => {
   try {
     const { name, link } = req.body;
     const card = await Card.create({ name, link, owner: req.user._id });
+    return res.status(201).send({ data: card });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Переданы некорректные данные', ...err });
+    }
+    return res.status(500).send({ message: 'Произошла неизвестная ошибка', ...err });
+  }
+};
+
+const deleteCard = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndDelete(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    );
     return res.status(200).send({ data: card });
   } catch (err) {
     if (err.name === 'ValidationError') {
@@ -42,7 +58,10 @@ const likeCard = async (req, res) => {
       { $addToSet: { likes: req.user._id } },
       { new: true },
     );
-    return res.status(200).send({ data: card });
+    if (card) {
+      return res.status(200).send({ data: card });
+    }
+    return res.status(404).send({ message: 'Карточка не найдена' });
   } catch (err) {
     if (err.name === 'CastError') {
       return res.status(404).send({ message: 'Карточка не найдена', ...err });
@@ -58,7 +77,10 @@ const dislikeCard = async (req, res) => {
       { $pull: { likes: req.user._id } },
       { new: true },
     );
-    return res.status(200).send({ data: card });
+    if (card) {
+      return res.status(200).send({ data: card });
+    }
+    return res.status(404).send({ message: 'Карточка не найдена' });
   } catch (err) {
     if (err.name === 'CastError') {
       return res.status(404).send({ message: 'Карточка не найдена', ...err });
@@ -71,6 +93,7 @@ module.exports = {
   getCards,
   getCardById,
   createCard,
+  deleteCard,
   likeCard,
   dislikeCard,
 };
