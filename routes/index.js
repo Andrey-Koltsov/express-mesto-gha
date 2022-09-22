a63a3129220d1bfb5bd95343e102bb8779f55a8c
Andrey Koltsov <a.koltcov@itr24.com>
2022-09-22 10:14:30 +0300

    fix: Исправить код ошибки не правильной страницы

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
-const BadRequestError = require('../errors/BadRequestError');
+const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 router.use((req, res, next) => {
-  next(new BadRequestError('Страница не найдена'));
+  next(new NotFoundError('Страница не найдена'));
 });
 
 module.exports = router;
